d91251273cc5a7c43832be80085f8762b5d73724
FPang95 <47764733+FPang95@users.noreply.github.com>
2019-07-31 09:59:15 -0400

    Update PokeWebscrape.py
    added new columns for pokemon typing and average base stats

--- PokemonShowdown/PokeWebscrape.py
+++ PokemonShowdown/PokeWebscrape.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen as uReq
 import numpy as np
 import PokeFunctions as pf
+import pandas as pd
 
 # name: Pokelog, password: DataTest
 
@@ -16,9 +17,12 @@ for r, d, f in os.walk(path):
 
 filename = "Poke_log.csv"
 f = open(filename, 'w')
-headers = "Tier,My Team/Pokemon Revealed,Opponent's Team/Pokemon Revealed,Win/Loss,Own Pokemon Down,Opponent " \
-          "Pokemon Down,My Moves Used,Opponent's Moves Used,Super effective,Not very effective," \
-          "My Attacking Moves,My Status Moves,Opponent's Attacking Moves,Opponent's Status Moves,Turns,Rank,Rank Range\n"
+headers = "Tier,Win/Loss,My Team/Pokemon Revealed,Opponent's Team/Pokemon Revealed,My Team Typing," \
+          "Opponent's Team Typing,Own Pokemon Down,Opponent Pokemon Down,My Moves Used,Opponent's Moves Used," \
+          "Super effective,Not very effective,My Attacking Moves,My Status Moves,Opponent's Attacking Moves," \
+          "Opponent's Status Moves,My Avg Total Stats,My Avg HP,My Avg Atk,My Avg Def,My Avg SpAtk," \
+          "My Avg SpDef,My Avg Speed,Opponent Avg Total Stats,Opponent Avg HP,Opponent Avg Atk," \
+          "Opponent Avg Def,Opponent Avg SpAtk,Opponent Avg SpDef, Opponent Avg Speed,Turns,Rank,Rank Range\n"
 f.write(headers)
 
 ###
@@ -35,6 +39,7 @@ moves = page_soup.find_all('td',{"width":"100px"})
 status_move = [x.text.strip() for x in moves]
 ###
 
+typing_df = pd.read_excel("Pokemon_list.xlsx")
 
 for pokefile in files:
     my_url = 'file:///Users/frankpang/PycharmProjects/PokeBattles/Pokelog/'+pokefile
@@ -54,15 +59,19 @@ for pokefile in files:
     # gives win or loss
     outcome = pf.winOrLose(page_soup)
 
-
     # gives rank
     elo = pf.getElo(page_soup, tier)
 
     elorange = pf.binRankings(str(elo))
 
+    myteamlist = pf.get_teams(page_soup, 0, tier)
+    opteamlist = pf.get_teams(page_soup, 1, tier)
     # gives teams
-    myteam = '|'.join(pf.get_teams(page_soup, 0, tier))
-    opteam = '|'.join(pf.get_teams(page_soup, 1, tier))
+    myteam = '|'.join(myteamlist)
+    opteam = '|'.join(opteamlist)
+
+    mytyping = '|'.join(pf.getTypings(typing_df, myteamlist))
+    optyping = '|'.join(pf.getTypings(typing_df, opteamlist))
 
     # gives turns
     turns = len(page_soup.findAll('h2',{"class":"battle-history"}))
@@ -79,16 +88,25 @@ for pokefile in files:
     oppoffense, oppstatus = pf.moveType(page_soup, status_move, 1)
 
     # give comprehensive list of all moves used
-    my_moves_list = "|".join(pf.moveList(page_soup,0))
-    opp_moves_list = "|".join(pf.moveList(page_soup,1))
+    my_moves_list = "|".join(pf.moveList(page_soup, 0))
+    opp_moves_list = "|".join(pf.moveList(page_soup, 1))
 
-    #move_counter = {x: total_moves.count(x) for x in total_moves}
-    #str_counter = str(move_counter)
-    #new_counter = str_counter.replace(", ", " | ")
+    MyAvgTotal,  MyAvgHP, MyAvgAttack, MyAvgDefense, MyAvgSpAtk, MyAvgSpDef, MyAvgSpeed = pf.baseStats(typing_df, myteamlist)
+    OpAvgTotal, OpAvgHP, OpAvgAttack, OpAvgDefense, OpAvgSpAtk, OpAvgSpDef, OpAvgSpeed = pf.baseStats(typing_df, opteamlist)
 
-    f.write(tier + "," + myteam + "," + opteam + "," + outcome + "," + str(own_poke_faints) + "," +
-            str(opp_poke_faints) + "," + my_moves_list + "," + opp_moves_list + "," + str(effective) +
-            "," + str(ineffective) + "," + str(myoffense) + "," + str(mystatus) + "," + str(oppoffense)
-            + "," + str(oppstatus) + "," + str(turns) + "," + str(elo) + "," + str(elorange) + "\n")
+    # move_counter = {x: total_moves.count(x) for x in total_moves}
+    # str_counter = str(move_counter)
+    # new_counter = str_counter.replace(", ", " | ")
+
+    f.write(tier + "," + outcome + "," + myteam + "," + opteam + "," + mytyping + "," + optyping + "," +
+            str(own_poke_faints) + "," + str(opp_poke_faints) + "," + my_moves_list + "," + opp_moves_list + ","
+            + str(effective) + "," + str(ineffective) + "," + str(myoffense) + "," + str(mystatus) + "," +
+            str(oppoffense) + "," + str(oppstatus) + "," + str(MyAvgTotal).strip("[]") + "," + str(MyAvgHP).strip("[]")
+            + "," + str(MyAvgAttack).strip("[]") + "," + str(MyAvgDefense).strip("[]") + "," +
+            str(MyAvgSpAtk).strip("[]") + "," + str(MyAvgSpDef).strip("[]") + "," + str(MyAvgSpeed).strip("[]") + ","
+            + str(OpAvgTotal).strip("[]") + "," + str(OpAvgHP).strip("[]") + "," + str(OpAvgAttack).strip("[]")
+            + "," + str(OpAvgDefense).strip("[]") + "," + str(OpAvgSpAtk).strip("[]") + "," +
+            str(OpAvgSpDef).strip("[]") + "," + str(OpAvgSpeed).strip("[]") + "," + str(turns) + "," + str(elo)
+            + "," + str(elorange) + "\n")
 
 f.close()
